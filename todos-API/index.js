339d9f07ea3b3c4fd1526185772c06e38ca54128
subreet26 <123537183+subreet26@users.noreply.github.com>
2024-11-17 18:04:45 -0800

    Update index.js

--- todos-API/index.js
+++ todos-API/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON bodies.
+// Middleware to parse JSON bodies
 app.use(express.json());
 
 
